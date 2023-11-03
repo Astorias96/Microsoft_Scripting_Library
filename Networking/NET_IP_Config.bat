72c9bfa1b01c06f558081b22e170a3bf19cad671
LemonTree <84917241+Astorias96@users.noreply.github.com>
2023-11-03 09:30:04 +0100

    Update and rename NET_IP_Config.bat to NET_IP_Config_Show.bat

--- Networking/NET_IP_Config.bat
+++
@@ -1,2 +0,0 @@
-@echo off > NUL && Powershell.exe -Executionpolicy  remotesigned -File  .\NET_IP_Config.ps1"
-pause
---
+++ Networking/NET_IP_Config_Show.bat
@@ -0,0 +1,2 @@
+@echo off > NUL && Powershell.exe -Executionpolicy  remotesigned -File  .\NET_IP_Config_Show.ps1"
+pause
