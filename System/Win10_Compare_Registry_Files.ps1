20011fb3e480ef7120a55438b3e4e533b2ba7f77
LemonTree <84917241+Astorias96@users.noreply.github.com>
2023-11-03 09:56:48 +0100

    Add files via upload

---
+++ System/Win10_Compare_Registry_Files.ps1
@@ -0,0 +1,111 @@
+﻿#### This Powershell script compares two registry files and sends the output to a file (the path is specified in the user variables)
+### Written on 02-11-2023 by Lemon Tree - Last edited on 02-11-2023 by Lemon Tree
+## Latest release comment - Initial release
+
+
+## Variables
+# User-defined variables - Basic settings
+$console_text_introduction = "Compare registry files utility"                                                                                                                                                                               # Defines the console introduction message (for administrators)
+$current_date = "date -f yyyy-MM-dd-HH:mm:ss"                                                                                                                                                                                               # Defines the date format to be used for the console output (for administrators)
+#$log_filepath = "C:\Temp\Win10_Compare_Registry_Files_Utility.log"                                                                                                                                                                         # Debugging - Defines the path to the logfile. Uncomment this line and both "Start/Stop-Transcript" lines to activate logging (lines 11, 33 and 114)
+$reg1_before_change = ""                                                                                                                                                                                                                    # Defines the path to the exported registry files that will be compared against - BEFORE the change - The script will prompt you during execution if left empty
+$reg2_after_change = ""                                                                                                                                                                                                                     # Defines the path to the exported registry files that will be compared against - AFTER the change - The script will prompt you during execution if left empty
+$reg_logfile = "C:\Temp\Win10_Compare_Registry_Files_Utility_Result.txt"                                                                                                                                                                    # Defines the path to the result file (containing the registry differences that were found) - The script will prompt you during execution if left empty
+
+# Exit codes
+$error_compare_registry = "2"                                                                                                                                                                                                               # The script ran into an error - The registry comparaison went into an error.
+$success = "0"                                                                                                                                                                                                                              # The script execution was successful.
+
+
+## Starting operations
+# Check if the script is running as an administrator
+$isAdmin = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).Groups -match "S-1-5-32-544"
+
+# If not running as administrator, restart with Run as Administrator
+if (-not $isAdmin) {
+    Start-Process -FilePath "powershell" -ArgumentList " -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
+
+## Debugging - Start transcript recording - Uncomment to activate logging
+#Start-Transcript -Path $log_filepath -Append
+
+
+# Sending date & time to console
+Write-Host "$console_text_introduction" -ForegroundColor Cyan
+Write-Host "The script has started, the timestamp is:"
+Invoke-Expression $current_date
+Write-Host ""
+
+## Verify that variables are set (and take necessary actions if they are not)
+# Check if $reg_before was specified in the user variables
+if ($reg_before) {
+    Write-Host "The registry filepath variable (BEFORE change) is set in the user variables." -ForegroundColor Green
+    Write-Host "The specified path is: $reg_before"
+    Write-Host ""
+}
+
+# Prompting the user for the variable if not set
+else {
+    Write-Host "The registry filepath variable (BEFORE change) is not set in the user variables." -ForegroundColor Yellow
+    Write-Host ""
+    $reg_before = Read-Host "Enter the filepath to the registry file (BEFORE change)"
+    Write-Host ""
+}
+
+# Check if $reg_after was specified in the user variables
+if ($reg_after) {
+    Write-Host "The registry file variable (AFTER change) is set in the user variables." -ForegroundColor Green
+    Write-Host "The specified path is: $reg_after"
+    Write-Host ""
+}
+
+# Prompting the user for the variable if not set
+else {
+    Write-Host "The registry file variable (AFTER change) is not set in the user variables." -ForegroundColor Yellow
+    Write-Host ""
+    $reg_after = Read-Host "Enter the filepath to the registry file (AFTER change)"
+    Write-Host ""
+}
+
+# Check if $reg_logfile was specified in the user variables
+if ($reg_after) {
+    Write-Host "The result logfile is set in the user variables." -ForegroundColor Green
+    Write-Host "The specified path is: $reg_logfile"
+    Write-Host ""
+}
+
+# Prompting the user for the variable if not set
+else {
+    Write-Host "The result logfile is not set in the user variables." -ForegroundColor Yellow
+    Write-Host ""
+    $reg_after = Read-Host "Enter the filepath to the desired result logfile"
+    Write-Host ""
+}
+
+## Compare the two registry files
+Write-Host "All variables are set, starting comparison. This can take several minutes, please keep this window open." -ForegroundColor Yellow
+Write-Host ""
+Compare-Object $(Get-Content "$reg_before") $(Get-Content "$reg_after") >> "$reg_logfile"
+
+# Checking latest command result
+if ($?) {
+    # Exit with success code
+    Write-host "The script has finished running. The result logfile was saved here:" -ForegroundColor Green
+    Write-Host "$reg_logfile"
+    Write-Host "`nExit." -ForegroundColor Green
+    Write-Host ""
+    Exit $success
+}
+
+# Exit if fail
+else {
+    Write-host "The script ran into an error. Press Enter to exit." -ForegroundColor Red; Read-Host
+    Write-Host ""
+    Exit $error_compare_registry
+}
+
+
+## Debugging - Stop transcript recording - Uncomment to activate logging
+#Stop-Transcript
---
+++ System/Win10_Disable_Automatic_Power_Settings.ps1
@@ -0,0 +1,137 @@
+﻿#### This Powershell script disables the automatic power settings (Disk, Hibernate, Monitor, Standby and Dynamic lock) on the target computer for the current user (using powercfg.exe and the registry)
+### Written on 03-11-2023 by Lemon Tree - Last edited on 03-11-2023 by Lemon Tree
+## Latest release comment - Initial release
+
+
+## Variables
+# User-defined variables - Basic settings
+$console_text_introduction = "Disable automatic power settings utility"                                                                                                                                                                     # Defines the console introduction message (for administrators)
+$current_date = "date -f yyyy-MM-dd-HH:mm:ss"                                                                                                                                                                                               # Defines the date format to be used for the console output (for administrators)
+#$log_filepath = "C:\Temp\Win10_Disable_Automatic_Power_Settings_Utility.log"                                                                                                                                                               # Debugging - Defines the path to the logfile. Uncomment this line and both "Start/Stop-Transcript" lines to activate logging (lines 11, 45 and 138)
+
+# System variables - Do not change
+$computer_disk_timeout_ac = "0"                                                                                                                                                                                                             # Defines the disk timeout when the computer is plugged-in (in minutes)
+$computer_disk_timeout_dc = "0"                                                                                                                                                                                                             # Defines the disk timeout when the computer is on battery power (in minutes)
+$computer_hibernate_timeout_ac = "0"                                                                                                                                                                                                        # Defines the hibernate timeout when the computer is plugged-in (in minutes)
+$computer_hibernate_timeout_dc = "0"                                                                                                                                                                                                        # Defines the hibernate timeout when the computer is on battery power (in minutes)
+$computer_monitor_timeout_ac = "0"                                                                                                                                                                                                          # Defines the display timeout when the computer is plugged-in (in minutes)
+$computer_monitor_timeout_dc = "0"                                                                                                                                                                                                          # Defines the display timeout when the computer is on battery power (in minutes)
+$computer_standby_timeout_ac = "0"                                                                                                                                                                                                          # Defines the sleep timeout when the computer is plugged-in (in minutes)
+$computer_standby_timeout_dc = "0"                                                                                                                                                                                                          # Defines the sleep timeout when the computer is on battery power (in minutes)
+$dynamicLockRegName = "EnableGoodbye"                                                                                                                                                                                                       # Defines the name of the dynamic lock registry key
+$dynamicLockRegValue = "0"                                                                                                                                                                                                                  # Defines the value of the dynamic lock registry key
+$localUsername = (Get-CimInstance -ClassName Win32_ComputerSystem).Username                                                                                                                                                                 # Determines the currently logged-in username (DOMAIN\USERNAME)
+$localUsernameSID = (New-Object System.Security.Principal.NTAccount($localUsername)).Translate([System.Security.Principal.SecurityIdentifier]).Value                                                                                        # Determines the currently logged-in username SID
+
+# Exit codes
+$error_configure_power_settings = "2"                                                                                                                                                                                                       # The script ran into an error - The power settings failed to configure for the current user.
+$error_registry_dynamic_lock = "3"                                                                                                                                                                                                          # The script ran into an error - The Windows dynamic lock registry failed to apply.
+$success = "0"                                                                                                                                                                                                                              # The script execution was successful.
+
+
+## Starting operations
+# Check if the script is running as an administrator
+$isAdmin = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).Groups -match "S-1-5-32-544"
+
+# If not running as administrator, restart with Run as Administrator
+if (-not $isAdmin) {
+    Start-Process -FilePath "powershell" -ArgumentList " -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
+
+## Debugging - Start transcript recording - Uncomment to activate logging
+#Start-Transcript -Path $log_filepath -Append
+
+
+# Sending date & time to console
+Write-Host "$console_text_introduction" -ForegroundColor Cyan
+Write-Host "The script has started, the timestamp is:"
+Invoke-Expression $current_date
+Write-Host ""
+
+
+## Configure timeout for automatic disk, hibernate, monitor and standby
+# Configure disk timeout (in minutes)
+powercfg.exe -x -disk-timeout-ac "$computer_disk_timeout_ac";
+powercfg.exe -x -disk-timeout-dc "$computer_disk_timeout_dc";
+
+# Configure hibernate timeout (in minutes)
+powercfg.exe -x -hibernate-timeout-ac "$computer_hibernate_timeout_ac";
+powercfg.exe -x -hibernate-timeout-dc "$computer_hibernate_timeout_dc";
+
+# Configure monitor timeout (in minutes)
+powercfg.exe -x -monitor-timeout-ac "$computer_monitor_timeout_ac";
+powercfg.exe -x -monitor-timeout-dc "$computer_monitor_timeout_dc";
+
+# Configure standby timeout (in minutes)
+powercfg.exe -x -standby-timeout-ac "$computer_standby_timeout_ac";
+powercfg.exe -x -standby-timeout-dc "$computer_standby_timeout_dc"
+
+# Checking latest command block result
+if ($?) {
+    Write-Host ""
+    Write-Host "All power settings were successfully configured." -ForegroundColor Green
+    Write-Host ""
+}
+
+# Exit if fail
+else {
+    Write-Host ""
+    Write-Host "Failed to configure the power settings for the current user. Press Enter to exit." -ForegroundColor Red; Read-Host
+    Write-Host ""
+    Exit $error_configure_power_settings
+}
+
+
+## Disable Dynamic lock using a registry key
+# Check if the registry hive/key exists and create it if not found
+$dynamicLockRegPath = "registry::HKEY_USERS\$localUsernameSID\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$dynamicLockRegPathDigest = "HKEY_USERS\$localUsernameSID\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+if (-not (Test-Path $dynamicLockRegPath)) {
+
+    # Create the registry key if it doesn't exist
+    New-Item -Path $dynamicLockRegPath -Force
+
+    # Checking latest command result
+    if ($?) {
+        Write-Host "Added the registry key: $dynamicLockRegPathDigest\$dynamicLockRegName with value $dynamicLockRegValue." -ForegroundColor Green
+        Write-Host ""
+    }
+                
+    # Exit if fail
+    else {
+        Write-Host "Failed to add the registry key: $dynamicLockRegPathDigest\$dynamicLockRegName with value $dynamicLockRegValue." -ForegroundColor Red
+        Write-Host ""
+        Write-Host "The script ran into an error. Press Enter to exit." -ForegroundColor Red; Read-Host
+        Write-Host ""
+        Exit $error_registry_dynamic_lock
+    }
+}
+
+# Set the registry value
+Set-ItemProperty -Path $dynamicLockRegPath -Name $dynamicLockRegName -Value $dynamicLockRegValue -Type DWORD
+
+# Checking latest command result
+if ($?) {
+    Write-Host "Added the registry key: $dynamicLockRegPathDigest\$dynamicLockRegName with value $dynamicLockRegValue." -ForegroundColor Green
+    Write-Host ""
+}
+            
+# Exit if fail
+else {
+    Write-Host "Failed to add the registry key: $dynamicLockRegPathDigest\$dynamicLockRegName with value $dynamicLockRegValue." -ForegroundColor Red
+    Write-Host ""
+    Write-Host "The script ran into an error. Press Enter to exit." -ForegroundColor Red; Read-Host
+    Write-Host ""
+    Exit $error_registry_dynamic_lock
+}
+
+# Exit with success code
+Write-host "The script has finished running. Exit." -ForegroundColor Green
+Write-Host ""
+Exit $success
+
+
+## Debugging - Stop transcript recording - Uncomment to activate logging
+#Stop-Transcript
